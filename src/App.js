8633c6deb7638ad979ab6e1d3e451bc4c5cc71ef
IMANARIYO <imanariyobaptiste@gmail.com>
2024-12-23 22:40:10 +0200

    adding the traffic making feature

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import LoginForm from "./components/userscomponent/Login";
 import ProfileManagementModule from "./components/dashboard/AboutMngt/AboutManagement";
 import ProjectTableModule from "./components/dashboard/ProjectsMngt/ProjectTable";
 import ProjectsSection from "./components/projects/Projects";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
 import ServiceManagementModule from "./components/dashboard/ServicesMngt/ServiceManagement";
 import Services from "./components/services/Services";
 import SignupForm from "./components/userscomponent/signup";
@@ -23,21 +25,34 @@ import SkillsTableModule from "./components/dashboard/AboutMngt/SkillsTable";
 import Testimonial from "./components/testimonies/Testimonial";
 import TestimonialsMngt from "./components/dashboard/TestimonyMngt/Testimonialmngt";
 import UsersManagement from "./components/dashboard/users/UsersManagement";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+
+// Initialize Google Analytics with your Measurement ID
+ReactGA.initialize("G-Y603TXHD3C"); // Replace G-XXXXXXX with your Measurement ID
+
+// Track page views on route changes
+const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+};
 
 function App() {
   return (
     <Router>
+      {/* Include Analytics to track page views */}
+      <Analytics />
       <Routes>
-        {/* Public Layout for Landing Page */}
         <Route path="/" element={<Layout />}>
-          {/* Default route for landing page */}
           <Route
             index
             element={
               <>
-
                 <Home />
                 <AboutMe />
                 <ProjectsSection />
@@ -50,56 +65,30 @@ function App() {
           />
         </Route>
         <Route path="login" element={<LoginForm />} />
-        <Route path="signup" element={<SignupForm/>} />
-
-         {/* <Route
-               path="/dashboard/*"
-                element={
-                    <Routes>
-                      <Route path="/"  element={<DashboardLayout />}>
-                      <Route index element={<DashboardSummary />} />
-                      <Route path="projects" element={<ProjectTableModule />} />
-                      <Route path="contacts" element={<ContactsManagementModule />} />
-                      <Route path="services" element={<ServiceManagementModule />} />
-                      <Route path="about" element={<AboutMe />} />
-                      <Route path="testimonials" element={<TestimonialsMngt/>} />
-                      <Route path="contacts" element={<ContactSection />} />
-                      <Route path="skills" element={<SkillsTableModule />} />
-                      <Route path="education" element={<EducationTableModule/>} />
-                      <Route path="experience" element={<ExperienceTableModule />} />
-                      <Route path="blogPostsManagement" element={<BlogTable />} />
-                      <Route path="profile-management" element={<ProfileManagementModule />} />
-                      <Route path="UsersManagement" element={<UsersManagement />} />
-                      </Route>
-                    
-                  
-                    </Routes>
-                }
-              /> */}
-              <Route
-        path="/dashboard/*"
-        element={
-          <AdminRoute> {/* Protect the dashboard routes */}
-            <Routes>
-              <Route path="/" element={<DashboardLayout />}>
-                <Route index element={<DashboardSummary />} />
-                <Route path="projects" element={<ProjectTableModule />} />
-                <Route path="contacts" element={<ContactsManagementModule />} />
-                <Route path="services" element={<ServiceManagementModule />} />
-                <Route path="about" element={<AboutMe />} />
-                <Route path="testimonials" element={<TestimonialsMngt />} />
-                <Route path="contacts" element={<ContactSection />} />
-                <Route path="skills" element={<SkillsTableModule />} />
-                <Route path="education" element={<EducationTableModule />} />
-                <Route path="experience" element={<ExperienceTableModule />} />
-                <Route path="blogPostsManagement" element={<BlogTable />} />
-                <Route path="profile-management" element={<ProfileManagementModule />} />
-                <Route path="UsersManagement" element={<UsersManagement />} />
-              </Route>
-            </Routes>
-          </AdminRoute>
-        }
-      />
+        <Route path="signup" element={<SignupForm />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            <AdminRoute>
+              <Routes>
+                <Route path="/" element={<DashboardLayout />}>
+                  <Route index element={<DashboardSummary />} />
+                  <Route path="projects" element={<ProjectTableModule />} />
+                  <Route path="contacts" element={<ContactsManagementModule />} />
+                  <Route path="services" element={<ServiceManagementModule />} />
+                  <Route path="about" element={<AboutMe />} />
+                  <Route path="testimonials" element={<TestimonialsMngt />} />
+                  <Route path="skills" element={<SkillsTableModule />} />
+                  <Route path="education" element={<EducationTableModule />} />
+                  <Route path="experience" element={<ExperienceTableModule />} />
+                  <Route path="blogPostsManagement" element={<BlogTable />} />
+                  <Route path="profile-management" element={<ProfileManagementModule />} />
+                  <Route path="UsersManagement" element={<UsersManagement />} />
+                </Route>
+              </Routes>
+            </AdminRoute>
+          }
+        />
       </Routes>
       <ToastContainer />
     </Router>
